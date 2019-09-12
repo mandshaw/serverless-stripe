9e16c577ab7a3e31515e5ec5f8e3b3741288c886
mandshaw <mikeshaw@stripe.com>
2019-09-12 12:18:50 +0100

    Adding required frontend files

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -1,21 +1,59 @@
 import React from "react";
+import { useState } from "react";
 import { injectStripe, Elements } from "react-stripe-elements";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
 import CardSection from "./CardSection";
+import axios from "axios";
 
 const RawForm = props => {
-  const handleSubmit = ev => {
+  const [error, setError] = useState();
+  const handleSubmit = async ev => {
     // We don't want to let default form submission happen here, which would refresh the page.
     ev.preventDefault();
-    console.log(props);
-    props.hidePaymentForm();
-    props.startPayment();
-    setTimeout(props.markCompleted, 1000);
+    const form = ev.target;
+    const userDetails = {
+      name: form.elements.formBasicName.value,
+      email: form.elements.formBasicEmail.value
+    };
+    try {
+      const paymentIntentClientSecret = await axios(
+        "/.netlify/functions/createPaymentIntent"
+      );
+      const { paymentIntent, error } = await props.stripe.handleCardPayment(
+        paymentIntentClientSecret.data.client_secret,
+        {
+          payment_method_data: {
+            billing_details: {
+              ...userDetails
+            }
+          }
+        }
+      );
+      if (error) {
+        setError(error);
+        console.log(error);
+      } else {
+        console.log(paymentIntent);
+        props.hidePaymentForm();
+        props.startPayment();
+        setTimeout(props.markCompleted, 1000);
+      }
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    }
   };
+
+  const paymentError = (
+    <Alert variant="danger">
+      <Alert.Heading>Error collecting Payment</Alert.Heading>
+      <p>{error ? error.message : ""}</p>
+    </Alert>
+  );
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formBasicPassword">
+      <Form.Group controlId="formBasicName">
         <Form.Label>Name</Form.Label>
         <Form.Control placeholder="First and Last Name" />
       </Form.Group>
@@ -27,8 +65,9 @@ const RawForm = props => {
         <Form.Label>Card Details</Form.Label>
         <CardSection />
       </Form.Group>
+      {error ? paymentError : ""}
       <Button variant="primary" type="submit">
-        Pay
+        Pay €9.99
       </Button>
     </Form>
   );
